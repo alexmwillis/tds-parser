0aa3842f21c8ddc94749d62d43f1927d976a85cf
Alex Willis <alexmwillis@outlook.com>
2017-01-01 22:27:33 +0000

    added more parsers

--- Parser.Test/ParsersTests.fs
+++ Parser.Test/ParsersTests.fs
@@ -16,10 +16,11 @@
           
         [<TestCase("{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}")>] 
         [<TestCase("0DE95AE4-41AB-4D01-9EB0-67441B7C2450")>] 
+        [<TestCase("0de95ae4-41ab-4d01-9eb0-67441b7c2450")>] 
         member t.``can parse valid guid`` (guid) =
             testParser parseGuid guid |> should equal (Guid guid)
             
-        [<TestCase("0de95ae4-41ab-4d01-9eb0-67441b7c2450")>] 
+        [<TestCase("0de95ae4-41ab-4d01-9eb0-67441b7Z2450")>] 
         member t.``can't parse invalid guid`` (guid) =
             (fun () -> testParser parseGuid guid |> ignore) |> should throw typeof<Exception>
 
@@ -28,32 +29,130 @@
           
         [<Test>] 
         member t.``can parse key with string value`` () =
-            let strToParse = "key: hello world\n"
-            testParser (parseKeyValueString "key") strToParse |> should equal "hello world"
+            let stringToParse = "key: hello world\n"
+            testParser (parseKeyValueString "key") stringToParse |> should equal "hello world"
             
         [<Test>] 
         member t.``can parse key with integer value`` () =
-            let strToParse = "key: 123\n"
-            testParser (parseKeyValueInt "key") strToParse |> should equal 123
+            let stringToParse = "key: 123\n"
+            testParser (parseKeyValueInt "key") stringToParse |> should equal 123
 
         [<Test>] 
         member t.``can parse key with guid value`` () =
-            let strToParse = "field: {A0CB3965-8884-4C7A-8815-B6B2E5CED162}"
-            testParser (parseKeyValueGuid "field") strToParse |> should equal (Guid "A0CB3965-8884-4C7A-8815-B6B2E5CED162")
+            let stringToParse = "field: {A0CB3965-8884-4C7A-8815-B6B2E5CED162}\r\n"
+            testParser (parseKeyValueGuid "field") stringToParse |> should equal (Guid "A0CB3965-8884-4C7A-8815-B6B2E5CED162")
+
+    [<TestFixture>]
+    type ``Parse content tests`` ()=
+
+        [<Test>]
+        member t.``can parse content`` ()=
+            let stringToParse = @"content-length: 12
+
+test-content
+"
+            let actualContent = testParser parseFieldContent stringToParse
+            actualContent |> should equal "test-content"
 
     [<TestFixture>]
     type ``Parse field tests`` ()=
           
         [<Test>] 
         member t.``can parse field`` () =
-            let field = @"----field----
+            let stringToParse = @"----field----
 field: {A0CB3965-8884-4C7A-8815-B6B2E5CED162}
 name: __Editors
 key: __editors
-content-length: 77
+content-length: 12
 
-{59F53BBB-D1F5-4E38-8EBA-0D73109BB59B}|{A0C460F4-DBAE-4A5A-8F3A-C4ADFCDACEEA}"
-            let expected = ("__editors", new Field(Guid "{A0CB3965-8884-4C7A-8815-B6B2E5CED162}", "__Editors", "__editors"))
-            testParser parseField field |> should equal expected
+test-content
+"
+            let expectedField =
+                { 
+                    fieldId = Guid "{A0CB3965-8884-4C7A-8815-B6B2E5CED162}";
+                    name = "__Editors";
+                    key = "__editors";
+                    value = "test-content"
+                    version = None
+                }
 
+            let actualField = testParser parseSharedField stringToParse 
+
+            actualField |> should equal expectedField
+
+        [<Test>] 
+        member t.``can parse field with no content`` () =
+            let stringToParse = @"----field----
+field: {1172F251-DAD4-4EFB-A329-0C63500E4F1E}
+name: __Masters
+key: __masters
+content-length: 0
+
+
+"            
+            let actualField = testParser parseSharedField stringToParse 
+
+            actualField.value |> should equal ""
             
+    [<TestFixture>]
+    type ``Parse versioned fields tests`` ()=
+
+        [<Test>]
+        member t.``can parse version`` () =
+            let stringToParse = "----version----
+language: en
+version: 1
+revision: 482b69be-2cd3-4d45-809e-015d7bf5ec5c
+
+"
+            let expectedVersion = { 
+                language = Language "en"; 
+                version = 1; 
+                revision = Guid "482b69be-2cd3-4d45-809e-015d7bf5ec5c"
+            } 
+            
+            let actualVersion = testParser parseVersion stringToParse 
+
+            actualVersion |> should equal expectedVersion
+                      
+        [<Test>] 
+        member t.``can parse versioned fields`` () =
+            let stringToParse = "----version----
+language: en
+version: 1
+revision: 482b69be-2cd3-4d45-809e-015d7bf5ec5c
+
+----field----
+field: {B5E02AD9-D56F-4C41-A065-A133DB87BDEB}
+name: __Display name
+key: __display name
+content-length: 7
+
+Content
+----field----
+field: {577F1689-7DE4-4AD2-A15F-7FDC1759285F}
+name: __Long description
+key: __long description
+content-length: 43
+
+This section contains all web site content.
+"
+            let expectedVersion = Some { language = Language "en"; version = 1; revision = Guid "482b69be-2cd3-4d45-809e-015d7bf5ec5c"} 
+            let expectedFields = [|
+                { 
+                    fieldId = Guid "{B5E02AD9-D56F-4C41-A065-A133DB87BDEB}";
+                    name = "__Display name";
+                    key = "__display name";
+                    value = "Content"
+                    version = expectedVersion
+                };
+                { 
+                    fieldId = Guid "{577F1689-7DE4-4AD2-A15F-7FDC1759285F}";
+                    name = "__Long description";
+                    key = "__long description";
+                    value = "This section contains all web site content."
+                    version = expectedVersion
+                }|]
+            let actualFields = testParser parseVersionedFields stringToParse 
+
+            actualFields |> should equal expectedFields
--- Parser/Parsers.fs
+++ Parser/Parsers.fs
@@ -2,43 +2,121 @@
 
     open System
     open FParsec
+    
+    let pipe11 p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 f =
+        pipe3 (tuple4 p1 p2 p3 p4) (tuple4 p5 p6 p7 p8) (tuple3 p9 p10 p11)
+            (fun (x1, x2, x3, x4) (x5, x6, x7, x8) (x9, x10, x11) -> f x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11)
 
-    type Field(field: Guid, name: string, key: string) =
-        member this.Field = field
-        member this.Name = name
-        member this.Key = string
-     
-    type Item = {
-        id : Guid
-        name : string
-        fields : Map<string, Field>
+    type Language = Language of string
+    type FieldName = FieldName of string
+            
+    type Version = {
+        version: Int32;
+        language: Language;
+        revision: Guid;
     }
+
+    let toVersion = fun language version revision -> 
+        { 
+            language = Language language; 
+            version = version; 
+            revision = revision 
+        }
+
+    type Field = {
+        fieldId: Guid; 
+        name: string;
+        key: string;
+        value: string;
+        version: option<Version>
+    }
+
+    let toField version = fun field name key content -> 
+        { 
+            fieldId = field; 
+            name = name; 
+            key = key; 
+            value = content; 
+            version = version 
+        }
+
+    type Item = {
+        version: Int32;
+        id: Guid;
+        database: string; 
+        path: string; 
+        parent: Guid; 
+        name: string; 
+        master: Guid; 
+        template: Guid; 
+        templatekey: string;
+        fields: List<Field>;
+    }
+
+    let toItem = fun version id database path parent name master template templatekey sharedFields versionedFields ->
+        {
+            version = version;
+            id = id;
+            database = database;
+            path = path;
+            parent = parent;
+            name = name;
+            master = master;
+            template = template;
+            templatekey = templatekey;
+            fields = List.concat [sharedFields; (List.collect (fun v -> v) versionedFields)];
+        }     
 
     type UserState = unit
     type Parser<'t> = Parser<'t, UserState>
 
     let parseGuid: Parser<_> =
-        let guidRegex = @"^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$"
-        regex guidRegex |>> fun a -> Guid.Parse a
+            let guidRegex = @"^[{(]?[0-9A-Fa-f]{8}[-]?([0-9A-Fa-f]{4}[-]?){3}[0-9A-Fa-f]{12}[)}]?"
+            regex guidRegex |>> fun a -> Guid.Parse a
+    
     let parseKeyValue key pvalue = pstring key >>. pstring ": " >>. pvalue
     let parseKeyValueString key = parseKeyValue key (restOfLine true)
-    let parseKeyValueInt key = parseKeyValue key pint16 .>> newline
-    let parseKeyValueGuid key = parseKeyValue key parseGuid
-    let parseField: Parser<_> = 
-        pstring "----field----" >>. newline >>. 
-        parseKeyValueGuid "field" .>>. 
-        parseKeyValueString "name" .>>. 
-        parseKeyValueString "key" |>> fun ((field, name), key) -> (key, new Field(field, name, key))
-           
-//let pItem = pstring "----item----" 
-//    >>. newline 
-//   .>>. pKeyValueInt "version" 
-//   .>>. pKeyValueGuid "id" 
-//   .>>. pKeyValueString "database" 
-//   .>>. pKeyValueString "path" 
-//   .>>. pKeyValueGuid "parent" 
-//   .>>. pKeyValueString "name" 
-//   .>>. pKeyValueString "master" 
-//   .>>. pKeyValueString "template" 
-//   .>>. pKeyValueString "templateKey" 
-//   .>>. many pField |>> fun ((id, name), fields) -> { id = id; name = name; fields = Map.ofList fields}
+    let parseKeyValueInt key = parseKeyValue key pint32 .>> newline
+    let parseKeyValueGuid key = parseKeyValue key parseGuid .>> newline
+    
+    let parseFieldContent: Parser<_> = 
+            (parseKeyValueInt "content-length" .>> newline) >>= fun contentLength -> (anyString contentLength .>> newline)
+    
+    let parseField version = 
+            pstring "----field----" >>. newline >>. 
+            pipe4 
+                (parseKeyValueGuid "field") 
+                (parseKeyValueString "name") 
+                (parseKeyValueString "key") 
+                parseFieldContent
+                (toField version)
+
+    let parseSharedField = parseField None
+
+    let parseVersion = 
+            pstring "----version----" >>. newline >>.
+            pipe3
+                (parseKeyValueString "language")
+                (parseKeyValueInt "version")
+                (parseKeyValueGuid "revision")
+                toVersion
+            .>> newline 
+
+    let parseVersionedFields =
+        parseVersion >>= fun version -> many (parseField (Some version))
+
+    let parseItem = 
+            pstring "----item----" >>. newline  >>.
+            pipe11
+                (parseKeyValueInt "version")
+                (parseKeyValueGuid "id")
+                (parseKeyValueString "database")
+                (parseKeyValueString "path")
+                (parseKeyValueGuid "parent")
+                (parseKeyValueString "name")
+                (parseKeyValueGuid "master")
+                (parseKeyValueGuid "template")
+                (parseKeyValueString "templateKey")
+                (many parseSharedField)
+                (many parseVersionedFields)
+                toItem
